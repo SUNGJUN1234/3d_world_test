002f4e70a8c30a53ca5e5e0449fa3a0a686940e9
SUNGJUN1234 <chotjdwns654@naver.com>
2023-03-10 16:11:45 +0900

    first commit

---
+++ back/test.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express();
+
+const path = require('path');
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.set('port',process.env.PORT||8888)
+
+app.get('/',(req,res)=>{
+    // __dirname = C:\Users\HAN\OneDrive\바탕 화면\workspace\portfolio\back
+    res.sendFile(path.join(__dirname,'../front/test.html'))
+})
+
+app.get('/get',(req,res)=>{
+    let id= req.query.id;
+    let pw = req.query.pw;
+
+    res.send('id : '+id+'pw : '+pw);
+})
+
+app.get('/three',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../front/three/SJ/text_geo.html'))
+})
+
+app.listen(app.get('port'),()=>{
+    console.log(app.get('port'),"번 포트에서 서버 연결 대기중...")
+    console.log(__dirname);
+})
